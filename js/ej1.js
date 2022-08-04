3bdbde4a7bdcd0b520cfe6f876b8a8232134551e
Eloy Pintos <eloy_pintos14@hotmail.com>
2022-08-04 13:39:17 -0300

    avance ejer1

---
+++ js/ej1.js
@@ -0,0 +1,31 @@
+
+
+function crearNumero(){
+    let numeroNuevo = document.querySelector("#numero");
+    numeroNuevo = Math.floor(Math.random() * (99 - 10 + 1) + 1);
+    console.log(numeroNuevo)
+
+    
+     let numeroUsuario = document.getElementById("usuarioNumero").value;
+ 
+
+   if(numeroNuevo === numeroUsuario){
+      alert("GANASTE!!! el numero que ingresaste es igual que el GANADOR")
+   }else if (numeroNuevo > numeroUsuario){
+     alert("El numero que ingresaste es menor al numero GANADOR")
+   }else{
+     alert("El numero que ingresaste es mayor al GANADOR")
+   }
+
+    
+ 
+
+}
+
+
+
+    
+
+
+
+
